0f355deb77fadc167923b480e6943c6502eaac03
Zack Voase <zack@meat.io>
2023-08-11 17:49:34 -0700

    Include weak mods in shiftstate.h.

--- keyboards/moonlander/keymaps/zoolander/shiftstate.h
+++ keyboards/moonlander/keymaps/zoolander/shiftstate.h
@@ -4,12 +4,15 @@
 #include "keycode.h"
 
 // Helpers for checking shift state and temporarily unshifting
-#define SHIFTED (get_mods() & MOD_MASK_SHIFT)
+#define SHIFTED (get_mods() & MOD_MASK_SHIFT || get_weak_mods() & MOD_MASK_SHIFT)
 
-#define UNSHIFT(...)                   \
-    {                                  \
-        uint8_t prevmods = get_mods(); \
-        del_mods(MOD_MASK_SHIFT);      \
-        __VA_ARGS__;                   \
-        set_mods(prevmods);            \
+#define UNSHIFT(...)                            \
+    {                                           \
+        uint8_t prevmods     = get_mods();      \
+        uint8_t prevweakmods = get_weak_mods(); \
+        del_mods(MOD_MASK_SHIFT);               \
+        del_weak_mods(MOD_MASK_SHIFT);          \
+        __VA_ARGS__;                            \
+        set_mods(prevmods);                     \
+        set_weak_mods(prevweakmods);            \
     }
