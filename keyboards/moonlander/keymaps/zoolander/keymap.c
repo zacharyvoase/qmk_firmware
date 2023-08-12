d865b278916a8a7ac0aaba03b06ae83c056aa569
Zack Voase <zack@meat.io>
2023-08-11 20:25:56 -0700

    Swap back grave and esc.

--- keyboards/moonlander/keymaps/zoolander/keymap.c
+++ keyboards/moonlander/keymaps/zoolander/keymap.c
@@ -167,11 +167,11 @@ tap_dance_action_t tap_dance_actions[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [INS] = LAYOUT_moonlander(
-    KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,          KC_5,       CW_TOGG, TT(SYM), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+    KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,          KC_5,       CW_TOGG, TT(SYM), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
     LT(MOV,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,       KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,          KC_G,       KC_LPRN, KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     TD(TD_SSHFT),   KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_MINS),
-    KC_ESC,         MO(FUN), KC_LCTL, KC_LALT, KC_LGUI,       TD(TD_AIRPODS),                       TO(GAM), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    KC_GRV,         MO(FUN), KC_LCTL, KC_LALT, KC_LGUI,       TD(TD_AIRPODS),                       TO(GAM), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
     KC_SPC,         MO(SYM), MO(LGT),                                                                           KC_RGUI, KC_BSLS, KC_SPC
     ),
 
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     KC_LSFT, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-    KC_GRV,  XXXXXXX, _______, _______, _______, _______,                           TO(INS), _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                           TO(INS), _______, _______, _______, _______, _______,
     _______, _______, _______,                                                                  _______, _______, _______
     ),
 
